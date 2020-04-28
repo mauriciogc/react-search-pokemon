0942eddaf595c68863daf7dbb6caf183969b19b8
mauriciogc <mauriciogc@gmail.com>
2020-04-28 12:26:10 -0500

    add ThemeProvider an Theme in App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,  {useState} from "react";
+import "./App.css"; //Change extension
+
+import { ThemeProvider } from "react-jss";
+
+const theme = {
+	light: {
+		background: "#FFF",
+		primary: "#FFF",
+		secondary: "#f8f8f8",
+		tertiary: "#F64360",
+		quaternary: "#B9334A",
+		color: "#474747",
+		boxShadow: "0 3px 5px ",
+		shadow: "#CBCBCB",
+		container: {
+			fontSize: "1rem",
+			margin: "auto",
+			marginBottom: "1.5rem",
+			padding: "1rem ",
+		},
+	},
+	dark: {
+		background: "#292C33",
+		primary: "#393A3F",
+		secondary: "#818491",
+		tertiary: "#5B9AF9",
+		quaternary: "#3C6BAF",
+		color: "#E3E3E3",
+		boxShadow: "0 3px 5px ",
+		shadow: "#1A1C20",
+		container: {
+			fontSize: "1rem",
+			margin: "auto",
+			marginBottom: "1.5rem",
+			padding: "1rem ",
+		},
+	},
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [typeTheme, setTypeTheme] = useState("dark");
+
+  const toogleTheme = () => {
+    setTypeTheme(typeTheme === "light" ? "dark" : "light");
+  };
+
+	return (
+		<ThemeProvider theme={{ theme: theme[typeTheme], toogleTheme }}>
+			<div className="App"></div>
+		</ThemeProvider>
+	);
 }
 
 export default App;
